087d49aafd23d0b49c834e8f3e788ea23d235278
kelvingy <kelvingy@udel.edu>
2021-11-04 19:41:49 -0400

    fix the error in auto deploy

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("First Year Fallr");
+  const linkElement = screen.getByText("First Year Fall");
   expect(linkElement).toBeInTheDocument();
 });
