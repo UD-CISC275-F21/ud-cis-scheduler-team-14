5dc42c390ccb8085e02d669075dd10960bfab966
lianghanfei1 <masonlhf@udel.edu>
2021-12-02 17:53:40 -0500

    update type issue

--- src/components/EditCourseForm.tsx
+++ src/components/EditCourseForm.tsx
@@ -1,38 +1,47 @@
 import React, { useState } from 'react'
+import { Form, Modal } from 'react-bootstrap'
+import { courseType } from '../interfaces/coursePool'
 
 interface editCourseForm{
     editTmpId:string;
-    editCourseForm: (tmpCourse: any) => void
+    editCourseForm: (tmpCourse: courseType) => void
+    setShowEditDiagram: React.Dispatch<React.SetStateAction<boolean>>
+    searchCourse: (id: string) => courseType
 }
 
-const EditCourseForm = ({editTmpId,editCourseForm}:editCourseForm) => {
+const EditCourseForm = ({editTmpId,editCourseForm,setShowEditDiagram, searchCourse}:editCourseForm) => {
     const [name,setName] = useState('')
     const [description, setDescription] = useState("")
-    const [credit, setCredit] = useState<number>()
+    const [credit, setCredit] = useState<number>(0)
 
-    
-    const onSubmitPlan = (e:any) =>{
+    const onSubmitPlan = (e:React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
         const id = editTmpId
+        let tmpCourse = searchCourse(id)
+        let prerequisite  = tmpCourse.prerequisite
+        let required = tmpCourse.required
+        let elective = tmpCourse.elective
 
-         editCourseForm({id,name,description,credit}) //why parameter name has to be the same as interface type name
-
+         editCourseForm({id,name,description,credit,prerequisite,required,elective}) //why parameter name has to be the same as interface type name
     }
     return (
-        <div >
-
-            <form onSubmit={onSubmitPlan} >
-                <p><label>course name</label></p>
-                <input type='text' placeholder='edit Course name' value={name} onChange={(e)=>setName(e.target.value)}/>
-                <p><label>description</label></p>
-                <input type='text' placeholder='edit description' value={description} onChange={(e)=>setDescription(e.target.value)}/>
-                <p><label>credit</label></p>
-                <input type='number' placeholder='edit credit'value={credit}  onChange={(e)=>setCredit(parseInt(e.target.value))}/>
-
-                <p><input type="submit" className="btn btn-primary m-2" value="save course"/></p>
-            </form>
-            
-        </div>
+        <Modal show={()=>setShowEditDiagram(true)} onHide={()=>setShowEditDiagram(false)} size="lg" centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Course: {editTmpId}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body >
+            <Form onSubmit={onSubmitPlan} >
+                <Form.Label>course name</Form.Label>
+                <p> <input type='text' placeholder='edit Course name' value={name} onChange={(e)=>setName(e.target.value)}/></p>
+                <Form.Label>description</Form.Label>
+                <p><input type='text' placeholder='edit description' value={description} onChange={(e)=>setDescription(e.target.value)}/></p>
+                <Form.Label>credit</Form.Label>
+                <p><input type='number' placeholder='edit credit'value={credit}  onChange={(e)=>setCredit(parseInt(e.target.value))}/></p>
+                <p><input type="submit" className="btn btn-success m-2" value="Save Change"/>
+                </p>
+            </Form>  
+        </Modal.Body>
+        </Modal>
     )
 }
 
