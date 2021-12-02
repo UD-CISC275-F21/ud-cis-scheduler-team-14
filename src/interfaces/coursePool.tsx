ef1f927f479c301ebd178a4391297f7685ca1b7b
lianghanfei1 <masonlhf@udel.edu>
2021-12-02 16:41:29 -0500

    update all types

--- src/interfaces/coursePool.tsx
+++ src/interfaces/coursePool.tsx
@@ -1,18 +1,35 @@
-export interface semesterCourses{
-    id: string;
-    name: string;
-    description: string;
-    credit: number;
-    prerequisite: string[];
-    required: boolean;
-    elective: boolean;
-}[]
-export interface course{
+export const defaultOb = {
+    "id":"not found",
+    "name":"",
+    "description":"",
+    "credit":0,
+    "prerequisite":[],
+    "required":false,
+    "elective":false
+  }
+export type courseType = {
     id:string
     name:string
-    description?:string
+    description:string
     credit:number
-    prerequisite:Array<string> //  ?? string[]
+    prerequisite:Array<string> 
     required:boolean
     elective:boolean
   }
+  export type semesterName = string
+  export type semesterCoursesType = courseType[]
+  export type semesterType = [semesterName,semesterCoursesType]
+  export type AllUserCoursesType = {
+    semesterName: string;
+    semesterCourses: courseType[];
+}[]
+
+// export interface semesterCourses{
+//     id: string;
+//     name: string;
+//     description: string;
+//     credit: number;
+//     prerequisite: string[];
+//     required: boolean;
+//     elective: boolean;
+// }[]
