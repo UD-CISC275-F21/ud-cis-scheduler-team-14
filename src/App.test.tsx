3608f00cd5b47b1cb7c2ecc0534a364067b35ddd
lianghanfei1 <masonlhf@udel.edu>
2021-12-07 18:05:16 -0500

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,12 +5,12 @@ import {defaultOb} from './interfaces/coursePool'
 
 describe("Tutorials",()=>{
     test('renders Tutorials text', () => {
-        render(<Tutorials />);
+        render(<Tutorials showTutorial={true} setShowTutorial={jest.fn()}/>);
         const TextElement = screen.getByText(/This is a four year degree plan/);
         expect(TextElement).toBeVisible();
       });
     test('renders Tutorials html tag', () => {
-        render(<Tutorials />);
+        render(<Tutorials showTutorial={true} setShowTutorial={jest.fn()}/>);
         const TagElement = screen.getByRole("heading");
         expect(TagElement).toBeInTheDocument();
       });
@@ -20,14 +20,14 @@ describe("AddCourseForm",  ()=>{
     test('input element placeholder',()=>{
         render(<AddCourseForm onAdd={mockedOnAdd} semesterPool = {[]}
             searchCourse={jest.fn()} checkPrerequisite={jest.fn()} defaultOb={defaultOb}
-            editDbCourse={jest.fn()}/>);
+            editDbCourse={jest.fn()} checkDuplicate={jest.fn()}/>);
         const InputBoxElement = screen.getByPlaceholderText(/Ex. CISC106/i)
         expect(InputBoxElement).toBeInTheDocument();
     })
     test('input element value change',()=>{
         render(<AddCourseForm onAdd={mockedOnAdd} semesterPool = {[]}
             searchCourse={jest.fn()} checkPrerequisite={jest.fn()} defaultOb={defaultOb}
-            editDbCourse={jest.fn()}/>);
+            editDbCourse={jest.fn()} checkDuplicate={jest.fn()}/>);
         const InputElement = screen.getByPlaceholderText(/Ex. CISC106/i) as  HTMLInputElement
         fireEvent.change(InputElement, {target:{value:"cisc106"}}) //after a event happen
         expect(InputElement.value).toBe("cisc106")
@@ -35,7 +35,7 @@ describe("AddCourseForm",  ()=>{
     test('input element not empty after search button is clicked',()=>{
         render(<AddCourseForm onAdd={mockedOnAdd} semesterPool = {[]}
             searchCourse={jest.fn()} checkPrerequisite={jest.fn()} defaultOb={defaultOb}
-            editDbCourse={jest.fn()}/>);
+            editDbCourse={jest.fn()}  checkDuplicate={jest.fn()}/>);
         const buttonElement = screen.getByRole("button", {name:/Search Course/i})
         const InputElement = screen.getByPlaceholderText(/Ex. CISC106/i) as  HTMLInputElement
         fireEvent.change(InputElement, {target:{value:"cisc106"}})
